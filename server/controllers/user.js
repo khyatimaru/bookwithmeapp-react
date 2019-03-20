5ee52694dc4ce025e6244d53306e0d188d3b26bb
khyatimaru <khyati.maru@gmail.com>
2019-03-20 13:05:58 +0000

    Adds register, adds login and auth middleware routes on server

---
+++ server/controllers/user.js
@@ -0,0 +1,127 @@
+const User = require('../models/user');
+const { normalizeErrors } = require('../helpers/mongoose');
+const jwt = require('jsonwebtoken');
+const config = require('../config/dev');
+
+exports.auth = function (req, res) {
+    const { email, password } = req.body;
+    
+    if(!email || !password) {
+        return res.status(422).send({errors: [{
+            title: 'Data missing!',
+            detail: 'Provide Email and Password!'
+        }]});
+    }
+    
+    User.findOne({email}, function (err, user) {
+        if(err) {
+            return res.status(422).send({errors: normalizeErrors(err.errors)});
+        }
+        if(!user) {
+            return res.status(422).send({errors: [{
+                title: 'Invalid user!',
+                detail: 'User does not exist!'
+            }]});
+        }
+        if(user.hasSamePassword(password)) {
+            // return jwt token
+            const token = jwt.sign({
+                userId: user.id,
+                username: user.username,
+            }, config.SECRET, { expiresIn: '1h' });
+
+            return res.json(token);
+        }
+        else {
+            return res.status(422).send({errors: [{
+                title: 'Wrong data!',
+                detail: 'Wrong email or password!'
+            }]});
+        }
+    });
+}
+
+exports.register = function (req, res) {
+
+    const { username, email, password, passwordConfirmation } = req.body;
+
+
+    if(!password || !email) {
+        return res.status(422).send({errors: [{
+            title: 'Data missing!',
+            detail: 'Provide Email and Password!'
+        }]});
+    }
+
+    if(password !== passwordConfirmation) {
+
+        return res.status(422).send({errors: [{
+            title: 'Invalid Password!',
+            detail: 'Password is not same as confirmation!'
+        }]});
+
+    }
+
+    User.findOne({email}, function (err, existingUser) {
+       if(err) {
+           return res.status(422).send({errors: normalizeErrors(err)});
+       }
+       if(existingUser) {
+           return res.status(422).send({errors: [{
+               title: 'Invalid email!',
+               detail: 'User with this email already exists!'
+           }]});
+       }
+
+        const user = new User({
+            username,
+            email,
+            password
+        });
+
+        user.save(function (err) {
+            if (err) {
+                return res.status(422).send({errors: normalizeErrors(err.errors)});
+            }
+            return res.json({'registered': true});
+        });
+
+    });
+
+}
+
+exports.authMiddleware = function(req, res, next) {
+    const token = req.headers.authorization;
+
+    if(token) {
+        const user = parseToken(token);
+
+        User.findById(user.userId, function(err, user) {
+           if(err) {
+               return res.status(422).send({errors: normalizeErrors(err.errors)});
+           }
+           if(user) {
+               res.locals.user = user;
+               next();
+           }
+           else {
+               return notAuthorized(res);
+           }
+        });
+    }
+    else {
+        return notAuthorized(res);
+    }
+}
+
+function parseToken(token) {
+    return jwt.verify(token.split(' ')[1], config.SECRET);
+
+}
+
+function notAuthorized(res) {
+    return res.status(401).send({errors: [{
+        title: 'Not authorized!',
+        detail: 'You need to login!'
+    }]});
+}
--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const config = require('./config/dev');
 const Rental = require('./models/rental');
 const FakeDb = require('./fake-db');
-const rentalRoutes = require('./routes/rentals');
+
+const rentalRoutes = require('./routes/rentals'),
+      userRoutes = require('./routes/users');
 
 mongoose.connect(config.DB_URI, { useNewUrlParser: true }).then(()=> {
     const fakeDb = new FakeDb();
@@ -12,7 +15,10 @@ mongoose.connect(config.DB_URI, { useNewUrlParser: true }).then(()=> {
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use('/api/v1/rentals', rentalRoutes);
+app.use('/api/v1/users', userRoutes);
 
 const PORT = process.env.PORT || 3001
 
