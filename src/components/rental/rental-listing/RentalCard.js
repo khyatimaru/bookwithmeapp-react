49172cff6f8173a6026543ef075a7a64961ea358
khyatimaru <khyati.maru@gmail.com>
2019-04-02 15:59:02 +0100

    replaced harcoded image path and moved bedrooms field below the image in create rental

--- src/components/rental/rental-listing/RentalCard.js
+++ src/components/rental/rental-listing/RentalCard.js
@@ -9,7 +9,7 @@ export function RentalCard(props) {
         <div className={ props.colNum }>
             <Link to={`/rentals/${rental._id}`} className='card-detail-link'>
                 <div className='card bookwithme-card'>
-                    <img className='card-img-top' src='http://via.placeholder.com/350x250' alt={rental.title}></img>
+                    <img className='card-img-top' src={rental.image} alt={rental.title}></img>
                     <div className='card-block'>
                         <h6 className={`card-subtitle ${rental.category}`}>{rentalType(rental.shared)} {rental.category} &#183; {rental.city}</h6>
                         <h4 className='card-title'>{rental.title}</h4>
